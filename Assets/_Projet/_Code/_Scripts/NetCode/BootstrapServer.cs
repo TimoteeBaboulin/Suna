7951178de0b211dfb85cb1a561390ae6476ffe87
Leonnel <leonnel.hammel06@gmail.com>
2025-01-06 14:52:34 +0100

    Fix bootstrap ECS
    Fix bootstrap ECS

--- Assets/_Projet/_Code/_Scripts/NetCode/BootstrapServer.cs
+++ Assets/_Projet/_Code/_Scripts/NetCode/BootstrapServer.cs
@@ -2,21 +2,20 @@ using UnityEngine;
 using Unity.Entities;
 using Unity.NetCode;
 using Unity.Networking.Transport;
+using UnityEngine.LightTransport;
 
-public partial struct BootstrapServer : ISystem
+public class BootstrapServer : ClientServerBootstrap
 {
-    public void OnUpdate(ref SystemState state)
+    public override bool Initialize(string defaultWorldName)
     {
-        state.Enabled = false;
-
         World serverWorld = ClientServerBootstrap.CreateServerWorld("ServerWorld");
-
         NetworkEndpoint serverEndpoint = NetworkEndpoint.AnyIpv4.WithPort(7979);
         {
             using EntityQuery networkDriverQuery = serverWorld.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
             networkDriverQuery.GetSingletonRW<NetworkStreamDriver>().ValueRW.Listen(serverEndpoint);
         }
-
-        Debug.Log("Server Start");
+        return true;
     }
 }
+
+
