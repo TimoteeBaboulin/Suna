c0013fd8f11486dbff166baefe5520e52cf80ee6
Leoleonmiel <leonnel.hammel06@gmail.com>
2025-01-07 15:23:43 +0100

    fix client
    Client is now created only once

--- Assets/_Projet/_Code/_Scripts/NetCode/Client/ClientConnection.cs
+++ Assets/_Projet/_Code/_Scripts/NetCode/Client/ClientConnection.cs
@@ -10,17 +10,31 @@ public class ClientConnection : MonoBehaviour
     [SerializeField] private string _adresse = "127.0.0.1";
     [SerializeField] private ushort _port = 7979;
 
+    private World _clientWorld;
+
 #if UNITY_CLIENT || UNITY_EDITOR
     public void ConnectClient()
     {
+        if (_clientWorld != null)
+        {
+            Debug.Log("Client is already created.");
+            return;
+        }
+
         DestroySimulationWorld();
 
-        World clientWorld = ClientServerBootstrap.CreateClientWorld("Client");
-
-        NetworkEndpoint connectionEndpoint = NetworkEndpoint.Parse(_adresse, _port);
+        _clientWorld = ClientServerBootstrap.CreateClientWorld("Client");
+        if (_clientWorld != null)
         {
-            using EntityQuery networkDriverQuery = clientWorld.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
-            networkDriverQuery.GetSingletonRW<NetworkStreamDriver>().ValueRW.Connect(clientWorld.EntityManager, connectionEndpoint);
+            NetworkEndpoint connectionEndpoint = NetworkEndpoint.Parse(_adresse, _port);
+            {
+                using EntityQuery networkDriverQuery = _clientWorld.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
+                networkDriverQuery.GetSingletonRW<NetworkStreamDriver>().ValueRW.Connect(_clientWorld.EntityManager, connectionEndpoint);
+            }
+        }
+        else
+        {
+            Debug.LogError("Failed to create client world.");
         }
     }
 #endif
