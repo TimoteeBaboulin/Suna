23503bcab8f53efc8bce0e3213aa206f2dd633f4
theodu30_ <121396182+theodu30@users.noreply.github.com>
2025-01-13 19:47:41 +0100

    Added KeyFunction To HUD Test
    + D to -5HP
    + H to +5HP
    + P to -5AP
    + U to +5AP
    + LMouse to -1Ammo
    + R to Reload Full Capacity
    + C to -10Cap
    + V to +10Cap

--- Assets/_Projet/_Code/_Scripts/UI/InGame/ConstantHUD/TestPlayerDataSystem.cs
+++ Assets/_Projet/_Code/_Scripts/UI/InGame/ConstantHUD/TestPlayerDataSystem.cs
@@ -1,7 +1,9 @@
 using System;
+using Unity.Burst;
 using Unity.Entities;
 using UnityEngine;
 
+[BurstCompile]
 public partial class TestPlayerDataSystem : SystemBase
 {
     public class HealthArgs : EventArgs { public uint Health; }
@@ -14,8 +16,81 @@ public partial class TestPlayerDataSystem : SystemBase
     public event EventHandler<AmmoArgs> OnAmmoChange;
     public event EventHandler<CapacityArgs> OnCapacityChange;
 
+    protected override void OnCreate()
+    {
+        RequireForUpdate<TestPlayerData>();
+    }
+
+    [BurstCompile]
     protected override void OnUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            foreach (RefRW<TestPlayerData> data in SystemAPI.Query<RefRW<TestPlayerData>>())
+            {
+                if (data.ValueRO.Health > 0) data.ValueRW.Health = data.ValueRO.Health - 5;
+                OnHealthChange?.Invoke(this, new HealthArgs { Health = data.ValueRO.Health });
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.H))
+        {
+            foreach (RefRW<TestPlayerData> data in SystemAPI.Query<RefRW<TestPlayerData>>())
+            {
+                if (data.ValueRO.Health < 100) data.ValueRW.Health = data.ValueRO.Health + 5;
+                OnHealthChange?.Invoke(this, new HealthArgs { Health = data.ValueRO.Health });
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            foreach (RefRW<TestPlayerData> data in SystemAPI.Query<RefRW<TestPlayerData>>())
+            {
+                if (data.ValueRO.Armor > 0) data.ValueRW.Armor = data.ValueRO.Armor - 5;
+                OnArmorChange?.Invoke(this, new ArmorArgs { Armor = data.ValueRO.Armor });
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.U))
+        {
+            foreach (RefRW<TestPlayerData> data in SystemAPI.Query<RefRW<TestPlayerData>>())
+            {
+                if (data.ValueRO.Armor < 95) data.ValueRW.Armor = data.ValueRO.Armor + 5;
+                OnArmorChange?.Invoke(this, new ArmorArgs { Armor = data.ValueRO.Armor });
+            }
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            foreach (RefRW<TestPlayerData> data in SystemAPI.Query<RefRW<TestPlayerData>>())
+            {
+                if (data.ValueRO.AmmoLeft > 0) data.ValueRW.AmmoLeft = data.ValueRO.AmmoLeft - 1;
+                OnAmmoChange?.Invoke(this, new AmmoArgs { Ammo = data.ValueRO.AmmoLeft });
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            foreach (RefRW<TestPlayerData> data in SystemAPI.Query<RefRW<TestPlayerData>>())
+            {
+                data.ValueRW.AmmoLeft = data.ValueRO.AmmoCapacity;
+                OnAmmoChange?.Invoke(this, new AmmoArgs { Ammo = data.ValueRO.AmmoLeft });
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            foreach (RefRW<TestPlayerData> data in SystemAPI.Query<RefRW<TestPlayerData>>())
+            {
+                if (data.ValueRO.AmmoCapacity > 10) data.ValueRW.AmmoCapacity = data.ValueRO.AmmoCapacity - 10;
+                data.ValueRW.AmmoLeft = data.ValueRO.AmmoCapacity;
+                OnCapacityChange?.Invoke(this, new CapacityArgs { Capacity = data.ValueRO.AmmoCapacity });
+                OnAmmoChange?.Invoke(this, new AmmoArgs { Ammo = data.ValueRO.AmmoLeft });
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.V))
+        {
+            foreach (RefRW<TestPlayerData> data in SystemAPI.Query<RefRW<TestPlayerData>>())
+            {
+                if (data.ValueRO.AmmoCapacity < 200) data.ValueRW.AmmoCapacity = data.ValueRO.AmmoCapacity + 10;
+                data.ValueRW.AmmoLeft = data.ValueRO.AmmoCapacity;
+                OnCapacityChange?.Invoke(this, new CapacityArgs { Capacity = data.ValueRO.AmmoCapacity });
+                OnAmmoChange?.Invoke(this, new AmmoArgs { Ammo = data.ValueRO.AmmoLeft });
+            }
+        }
     }
 }
