f42a1b835bd5bc231f29bd328d74e6bcc292f1a2
Smalleater <163728051+Smalleater@users.noreply.github.com>
2025-01-25 17:48:34 +0100

    Update Health system
    The healing system now uses jobs and is burstcompiled.

--- Assets/_Projet/_Code/_Scripts/NetCode/Common/CommonHealth.cs
+++ Assets/_Projet/_Code/_Scripts/NetCode/Common/CommonHealth.cs
@@ -1,6 +1,8 @@
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.NetCode;
+using UnityEngine;
 
 public struct MaxHealthComponent : IComponentData
 {
@@ -27,85 +29,118 @@ public struct DamageThisTickCommand : ICommandData
 
 public struct HasNoHealthTag : IComponentData { }
 
-
 [UpdateInGroup(typeof(PredictedSimulationSystemGroup), OrderLast = true)]
+[BurstCompile]
 public partial struct CalculateFrameDamageSystem : ISystem
 {
     public void OnCreate(ref SystemState state)
     {
+        EntityQueryBuilder builder = new EntityQueryBuilder(Allocator.Temp);
+        builder.WithAll<DamageBufferElement, DamageThisTickCommand, Simulate>();
+
         state.RequireForUpdate<NetworkTime>();
+        state.RequireForUpdate(state.GetEntityQuery(builder));
     }
 
     public void OnUpdate(ref SystemState state)
     {
         NetworkTick currentTick = SystemAPI.GetSingleton<NetworkTime>().ServerTick;
 
-        foreach (var (damageBuffer, damageThisTickBuffer) in SystemAPI
-            .Query<DynamicBuffer<DamageBufferElement>, DynamicBuffer<DamageThisTickCommand>>()
-            .WithAll<Simulate>())
+        CalculateFrameDamageJob job = new CalculateFrameDamageJob
         {
-            if (damageBuffer.IsEmpty)
+            CurrentTick = currentTick
+        };
+        state.Dependency = job.ScheduleParallel(state.Dependency);
+    }
+}
+
+[BurstCompile]
+public partial struct CalculateFrameDamageJob : IJobEntity
+{
+    [ReadOnly] public NetworkTick CurrentTick;
+
+    public void Execute(DynamicBuffer<DamageBufferElement> damageBuffer, DynamicBuffer<DamageThisTickCommand> damageThisTickBuffer)
+    {
+        if (damageBuffer.IsEmpty)
+        {
+            damageThisTickBuffer.AddCommandData(new DamageThisTickCommand { Tick = CurrentTick, Value = 0 });
+        }
+        else
+        {
+            int totalDamage = 0;
+
+            if (damageThisTickBuffer.GetDataAtTick(CurrentTick, out var damageThisTick))
             {
-                damageThisTickBuffer.AddCommandData(new DamageThisTickCommand { Tick = currentTick, Value = 0 });
+                totalDamage = damageThisTick.Value;
             }
-            else
+
+            foreach (var damage in damageBuffer)
             {
-                int totalDamage = 0;
-
-                if (damageThisTickBuffer.GetDataAtTick(currentTick, out var damageThisTick))
-                {
-                    totalDamage = damageThisTick.Value;
-                }
-
-                foreach (var damage in damageBuffer)
-                {
-                    totalDamage += damage.Value;
-                }
-
-                damageThisTickBuffer.AddCommandData(new DamageThisTickCommand { Tick = currentTick, Value = totalDamage });
-                damageBuffer.Clear();
+                totalDamage += damage.Value;
             }
+
+            damageThisTickBuffer.AddCommandData(new DamageThisTickCommand { Tick = CurrentTick, Value = totalDamage });
+            damageBuffer.Clear();
         }
     }
 }
 
 [UpdateInGroup(typeof(PredictedSimulationSystemGroup), OrderLast = true)]
 [UpdateAfter(typeof(CalculateFrameDamageSystem))]
+[BurstCompile]
 public partial struct ApplyDamageSystem : ISystem
 {
     public void OnCreate(ref SystemState state)
     {
+        EntityQueryBuilder builder = new EntityQueryBuilder(Allocator.Temp);
+        builder.WithAll<CurrentHealthComponent, DamageThisTickCommand, Simulate>();
+
         state.RequireForUpdate<NetworkTime>();
+        state.RequireForUpdate(state.GetEntityQuery(builder));
     }
 
     public void OnUpdate(ref SystemState state)
     {
         NetworkTick currentTick = SystemAPI.GetSingleton<NetworkTime>().ServerTick;
-        EntityCommandBuffer ecb = new EntityCommandBuffer(Allocator.Temp);
+        EntityCommandBuffer ecb = new EntityCommandBuffer(Allocator.TempJob);
 
-        foreach (var (currentHealth, damageThisTickBuffer, entity) in SystemAPI
-            .Query<RefRW<CurrentHealthComponent>, DynamicBuffer<DamageThisTickCommand>>()
-            .WithAll<Simulate>()
-            .WithEntityAccess())
+        ApplyDamageJob job = new ApplyDamageJob
         {
-            if (!damageThisTickBuffer.GetDataAtTick(currentTick, out var damageThisTick))
-            {
-                continue;
-            }
+            CurrentTick = currentTick,
+            ECB = ecb.AsParallelWriter()
+        };
+        state.Dependency = job.ScheduleParallel(state.Dependency);
 
-            if (damageThisTick.Tick != currentTick)
-            {
-                continue;
-            }
-
-            currentHealth.ValueRW.Value -= damageThisTick.Value;
-
-            if (currentHealth.ValueRO.Value <= 0)
-            {
-                ecb.AddComponent<HasNoHealthTag>(entity);
-            }
-        }
+        state.Dependency.Complete();
 
         ecb.Playback(state.EntityManager);
+        ecb.Dispose();
+    }
+}
+
+[BurstCompile]
+public partial struct ApplyDamageJob : IJobEntity
+{
+    [ReadOnly] public NetworkTick CurrentTick;
+    public EntityCommandBuffer.ParallelWriter ECB;
+
+    public void Execute(Entity entity, [EntityIndexInQuery] int sortKey, RefRW<CurrentHealthComponent> currentHealth, DynamicBuffer<DamageThisTickCommand> damageThisTickBuffer)
+    {
+        if (!damageThisTickBuffer.GetDataAtTick(CurrentTick, out var damageThisTick))
+        {
+            return;
+        }
+
+        if (damageThisTick.Tick != CurrentTick)
+        {
+            return;
+        }
+
+        currentHealth.ValueRW.Value -= damageThisTick.Value;
+
+        if (currentHealth.ValueRO.Value <= 0)
+        {
+            ECB.AddComponent<HasNoHealthTag>(sortKey, entity);
+        }
     }
 }
