17d316811f88b1db243322c5606814fab3d7fa3c
Smalleater <163728051+Smalleater@users.noreply.github.com>
2025-01-27 11:59:22 +0100

    Update InGameHUDSystem

--- Assets/_Projet/_Code/_Scripts/UI/InGame/ConstantHUD/InGameHUDSystem.cs
+++ Assets/_Projet/_Code/_Scripts/UI/InGame/ConstantHUD/InGameHUDSystem.cs
@@ -1,5 +1,6 @@
 using System;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.NetCode;
 
@@ -8,21 +9,30 @@ partial class InGameHUDSystem : SystemBase
 {
     public class HealthArgs : EventArgs { public int Health; }
     public event EventHandler<HealthArgs> HealthChangedEvent;
+    public event EventHandler HitRegister;
 
     [BurstCompile]
     protected override void OnCreate()
     {
-        RequireForUpdate<CurrentHealthComponent>();
+        EntityQueryBuilder builder = new EntityQueryBuilder(Allocator.Temp);
+        builder.WithAll<CurrentHealthComponent, HasHitComponent>();
+
+        RequireForUpdate(GetEntityQuery(builder));
     }
 
     [BurstCompile]
     protected override void OnUpdate()
     {
-        foreach (var currentHealth in SystemAPI
-            .Query<RefRO<CurrentHealthComponent>>()
+        foreach (var (currentHealth, hasHit) in SystemAPI
+            .Query<RefRO<CurrentHealthComponent>, RefRO<HasHitComponent>>()
             .WithAll<GhostOwnerIsLocal>())
         {
             HealthChangedEvent?.Invoke(this, new HealthArgs { Health = currentHealth.ValueRO.Value });
+
+            if (hasHit.ValueRO.Value)
+            {
+                HitRegister?.Invoke(this, EventArgs.Empty);
+            }
         }
     }
 }
