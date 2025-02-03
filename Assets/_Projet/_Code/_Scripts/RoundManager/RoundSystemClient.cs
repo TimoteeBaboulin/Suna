fe5f60f495f0f182db1407b9cd2ca768e421b37a
Timotee Baboulin <timotee.baboulin@gmail.com>
2025-02-03 15:12:36 +0100

    Fixed Incomplete RoundManagerClient Script

--- Assets/_Projet/_Code/_Scripts/RoundManager/RoundSystemClient.cs
+++ Assets/_Projet/_Code/_Scripts/RoundManager/RoundSystemClient.cs
@@ -3,6 +3,7 @@ using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.NetCode;
+using Unity.Transforms;
 using UnityEngine;
 using static RoundSystemServer;
 
@@ -103,3 +104,6 @@ partial struct RoundSystemClient : IRoundManager, ISystem
         
     }
 }
+
+
+
