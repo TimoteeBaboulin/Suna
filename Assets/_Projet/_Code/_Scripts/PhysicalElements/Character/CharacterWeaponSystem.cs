cdd617c3dfc3b2239a83c93a4b4fc1c67157ee73
Smalleater <163728051+Smalleater@users.noreply.github.com>
2025-03-01 22:57:40 +0100

    The weapon no longer disappears after 3 frames.

--- Assets/_Projet/_Code/_Scripts/PhysicalElements/Character/CharacterWeaponSystem.cs
+++ Assets/_Projet/_Code/_Scripts/PhysicalElements/Character/CharacterWeaponSystem.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 
 public struct StuffIsInstanciedTag : IComponentData { }
 
-[WorldSystemFilter(WorldSystemFilterFlags.ClientSimulation)]
+public struct CharacterWeaponTag : IComponentData { }
+
+[WorldSystemFilter(WorldSystemFilterFlags.ServerSimulation)]
 partial struct CharacterWeaponSystem : ISystem
 {
     public void OnCreate(ref SystemState state)
     {
         EntityQueryBuilder builder = new EntityQueryBuilder(Allocator.Temp);
-        builder.WithAll<CharacterDefaultWeaponPrefab>().WithAbsent<StuffIsInstanciedTag>();
+        builder.WithAll<CharacterDefaultWeaponPrefab, CharacterDefaultWeapon>().WithNone<StuffIsInstanciedTag>();
         state.RequireForUpdate(state.GetEntityQuery(builder));
     }
 
@@ -25,8 +27,10 @@ partial struct CharacterWeaponSystem : ISystem
         {
             if (prefab.ValueRO.Value != Entity.Null)
             {
-                weapon.ValueRW.Value = ecb.Instantiate(prefab.ValueRO.Value);
-                ecb.AddComponent(weapon.ValueRW.Value, new Parent { Value = charaEntity });
+                //weapon.ValueRW.Value = ecb.Instantiate(prefab.ValueRO.Value);
+                Entity entity = ecb.Instantiate(prefab.ValueRO.Value);
+                ecb.AddComponent(entity, new Parent { Value = charaEntity });
+                ecb.AddComponent<CharacterDefaultWeaponPrefab>(entity);
 
                 //ecb.AddBuffer<Child>(charaEntity);
                 //ecb.AppendToBuffer(charaEntity, new Child { Value = stuffs.ValueRW.mainWeapon });
@@ -36,5 +40,18 @@ partial struct CharacterWeaponSystem : ISystem
 
         ecb.Playback(state.EntityManager);
         ecb.Dispose();
+
+        foreach (var (parent, entity) in SystemAPI
+            .Query<RefRO<Parent>>()
+            .WithAll<CharacterWeaponTag>()
+            .WithEntityAccess())
+        {
+            RefRW<CharacterDefaultWeapon> weapon = SystemAPI.GetComponentRW<CharacterDefaultWeapon>(parent.ValueRO.Value);
+
+            if (weapon.ValueRW.Value == Entity.Null)
+            {
+                weapon.ValueRW.Value = entity;
+            }
+        }
     }
 }
