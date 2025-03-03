2462a750e6f2182b1db28fd98870b72cabb240db
Timotee Baboulin <timotee.baboulin@gmail.com>
2025-03-03 16:17:48 +0100

    Revert a revert

--- Assets/_Projet/_Code/_Scripts/PhysicalElements/Stuff/Weapons/RangedWeaponAuthoring.cs
+++ Assets/_Projet/_Code/_Scripts/PhysicalElements/Stuff/Weapons/RangedWeaponAuthoring.cs
@@ -2,6 +2,11 @@ using Unity.Entities;
 using Unity.Entities.Serialization;
 using UnityEngine;
 
+public class RangedWeaponDataRef : IComponentData
+{
+    public RangedWeaponData Value;
+}
+
 public struct ModifiersComponent : IComponentData
 {
     public Entity scope;
@@ -25,56 +30,55 @@ public class RangedWeaponAuthoring : MonoBehaviour
 
             Entity entity = GetEntity(TransformUsageFlags.Dynamic);
 
-            AddComponent(entity, new PrefabReferenceComponent
+            AddComponentObject(entity, new RangedWeaponDataRef
             {
-                prefab = GetEntity(d.prefab, TransformUsageFlags.Dynamic)
+                Value = d,
             });
 
-            AddComponent(entity, new StuffInfosComponent
-            {
-                entityName = d.entityName,
-                price = d.price,
-                type = d.type,
-                side = d.side,
-                deploymentSpeed = d.deploymentSpeed,
-                storageSpeed = d.storageSpeed
-            });
-
-            AddComponent(entity, new RangedWeaponComponent
-            {
-                recoil = d.recoil,
-                damage = d.damage,
-                range = d.range,
-                spread = d.spread,
-                spreadAiming = d.spreadAiming,
-                coefSpray = d.coefSpray,
-                coefSprayAiming = d.coefSprayAiming,
-                ergonomics = d.ergonomics,
-                roundsPerMin = d.roundsPerMin,
-                dmgFallOff = d.dmgFallOff,
-                coefModifMoveSpeed = d.coefModifMoveSpeed,
-                coefModifMoveSpeedAiming = d.coefModifMoveSpeedAiming,
-                reloadSpeed = d.reloadSpeed,
-                fastReloadSpeed = d.fastReloadSpeed,
-                knockbackForceOnKill = d.knockbackForceOnKill,
-                nbMagazine = d.nbMagazine,
-                magazineCapacity = d.magazineCapacity,
-
-                thorax = d.thorax,
-                stomach = d.stomach,
-                legs_Arms = d.stomach,
-                head = d.head
-            });
-
-            AddComponent(entity, new ModifiersComponent
-            {
-
-                scope = d.scope != null ? GetEntity(d.scope.prefab, TransformUsageFlags.Dynamic) : default,
-                handle = d.handle != null ? GetEntity(d.handle.prefab, TransformUsageFlags.Dynamic) : default,
-                cross = d.cross != null ? GetEntity(d.cross.prefab, TransformUsageFlags.Dynamic) : default,
-                silencer = d.silencer != null ? GetEntity(d.silencer.prefab, TransformUsageFlags.Dynamic) : default,
-                magazine = d.magazine != null ? GetEntity(d.magazine.prefab, TransformUsageFlags.Dynamic) : default,
-            });
+            //AddComponent(entity, new ModifiersComponent
+            //{
+            //    scope = d.scope != null ? GetEntity(d.scope.prefab, TransformUsageFlags.Dynamic) : default,
+            //    handle = d.handle != null ? GetEntity(d.handle.prefab, TransformUsageFlags.Dynamic) : default,
+            //    cross = d.cross != null ? GetEntity(d.cross.prefab, TransformUsageFlags.Dynamic) : default,
+            //    silencer = d.silencer != null ? GetEntity(d.silencer.prefab, TransformUsageFlags.Dynamic) : default,
+            //    magazine = d.magazine != null ? GetEntity(d.magazine.prefab, TransformUsageFlags.Dynamic) : default,
+            //});
         }
     }
 }
+
+            //AddComponent(entity, new StuffInfosComponent
+            //{
+            //    entityName = d.entityName,
+            //    price = d.price,
+            //    type = d.type,
+            //    side = d.side,
+            //    deploymentSpeed = d.deploymentSpeed,
+            //    storageSpeed = d.storageSpeed
+            //});
+
+            //AddComponent(entity, new RangedWeaponComponent
+            //{
+            //    recoil = d.recoil,
+            //    damage = d.damage,
+            //    range = d.range,
+            //    spread = d.spread,
+            //    spreadAiming = d.spreadAiming,
+            //    coefSpray = d.coefSpray,
+            //    coefSprayAiming = d.coefSprayAiming,
+            //    ergonomics = d.ergonomics,
+            //    roundsPerMin = d.roundsPerMin,
+            //    dmgFallOff = d.dmgFallOff,
+            //    coefModifMoveSpeed = d.coefModifMoveSpeed,
+            //    coefModifMoveSpeedAiming = d.coefModifMoveSpeedAiming,
+            //    reloadSpeed = d.reloadSpeed,
+            //    fastReloadSpeed = d.fastReloadSpeed,
+            //    knockbackForceOnKill = d.knockbackForceOnKill,
+            //    nbMagazine = d.nbMagazine,
+            //    magazineCapacity = d.magazineCapacity,
+
+            //    thorax = d.thorax,
+            //    stomach = d.stomach,
+            //    legs_Arms = d.stomach,
+            //    head = d.head
+            //});
