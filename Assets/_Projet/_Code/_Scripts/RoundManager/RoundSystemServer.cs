a118275036d4b420ae19fc82d3f2687c619e5401
Timotee Baboulin <timotee.baboulin@gmail.com>
2025-03-10 14:19:06 +0100

    Clean-Up

--- Assets/_Projet/_Code/_Scripts/RoundManager/RoundSystemServer.cs
+++ Assets/_Projet/_Code/_Scripts/RoundManager/RoundSystemServer.cs
@@ -1,9 +1,7 @@
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
-using Unity.Mathematics;
 using Unity.NetCode;
-using Unity.Transforms;
 using UnityEngine;
 using static RoundSystemClient;
 
