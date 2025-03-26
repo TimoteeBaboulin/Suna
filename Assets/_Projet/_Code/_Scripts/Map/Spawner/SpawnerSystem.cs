985e4089f88482f4440a8c1ea037b6258e523868
Smalleater <163728051+Smalleater@users.noreply.github.com>
2025-03-26 09:25:24 +0100

    Temp patch for respawn

--- Assets/_Projet/_Code/_Scripts/Map/Spawner/SpawnerSystem.cs
+++ Assets/_Projet/_Code/_Scripts/Map/Spawner/SpawnerSystem.cs
@@ -62,7 +62,7 @@ public partial struct OnDieJob : IJobEntity
             commandBuffer.SetComponentEnabled<CharacterEnableTag>(sortKey, entity, false);
             commandBuffer.AddComponent<WaitForRespawnTag>(sortKey, CharacterPlayerAttached.ValueRO.ClientEntity);
             commandBuffer.RemoveComponent<HasNoHealthTag>(sortKey, entity);
-            //commandBuffer.DestroyEntity(sortKey, entity);
+            commandBuffer.DestroyEntity(sortKey, entity);
 
             //commandBuffer.AddComponent<ResetStuffTag>(sortKey, entity);
         }
@@ -134,24 +134,27 @@ public partial struct RespawnSystem : ISystem
 
             int networkId = state.EntityManager.GetComponentData<GhostOwner>(entity).NetworkId;
 
-            //Spawn a new character if the client no longer has one, otherwise teleport it back to the start with full health
-            Entity characterEntity = SystemAPI.GetComponent<ClientCharacterAttached>(entity).Value;
-            if (!state.EntityManager.Exists(characterEntity))
-            {
-                SpawnCharacter(entity, networkId, ecb, buffer[random]);
-                ecb.RemoveComponent<WaitForRespawnTag>(entity);
-            }
-            else
-            {
-                RefRW<LocalTransform> transform = SystemAPI.GetComponentRW<LocalTransform>(characterEntity);
-                RefRW<CurrentHealthComponent> currentHealth = SystemAPI.GetComponentRW<CurrentHealthComponent>(characterEntity);
-                transform.ValueRW.Position = buffer[random];
-                currentHealth.ValueRW.Value = 100;
-
-                ecb.SetComponentEnabled<CharacterEnableTag>(characterEntity, true);
-            }
-
+            SpawnCharacter(entity, networkId, ecb, buffer[random]);
             ecb.RemoveComponent<WaitForRespawnTag>(entity);
+
+            //Spawn a new character if the client no longer has one, otherwise teleport it back to the start with full health
+            //Entity characterEntity = SystemAPI.GetComponent<ClientCharacterAttached>(entity).Value;
+            //if (!state.EntityManager.Exists(characterEntity))
+            //{
+            //    SpawnCharacter(entity, networkId, ecb, buffer[random]);
+            //    ecb.RemoveComponent<WaitForRespawnTag>(entity);
+            //}
+            //else
+            //{
+            //    RefRW<LocalTransform> transform = SystemAPI.GetComponentRW<LocalTransform>(characterEntity);
+            //    RefRW<CurrentHealthComponent> currentHealth = SystemAPI.GetComponentRW<CurrentHealthComponent>(characterEntity);
+            //    transform.ValueRW.Position = buffer[random];
+            //    currentHealth.ValueRW.Value = 100;
+
+            //    ecb.SetComponentEnabled<CharacterEnableTag>(characterEntity, true);
+            //}
+
+            //ecb.RemoveComponent<WaitForRespawnTag>(entity);
         }
     }
 
