a15b2f81bf31a0b4ddfc0ab2d57804e0d42c32ac
Arca Théo <121396182+theodu30@users.noreply.github.com>
2025-03-27 17:01:14 +0100

    Continued on Shop
    + New Util Class to make life easier for UIToolkit
    + Added new functions to add buttons and lines in shop menu
    + Added base layer for stat window

--- Assets/_Projet/_Code/_Scripts/UI/InGame/StuffShop/ShopController.cs
+++ Assets/_Projet/_Code/_Scripts/UI/InGame/StuffShop/ShopController.cs
@@ -1,10 +1,9 @@
 using System.Collections.Generic;
-using System.Linq;
-using Unity.Collections;
-using Unity.Entities;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+
+using UI = UIDocumentUtils;
+
 
 public class ShopController : MonoBehaviour
 {
@@ -13,43 +12,111 @@ public class ShopController : MonoBehaviour
     private VisualElement shop;
     private VisualElement shopmenu;
 
+    [SerializeField] private RangedWeaponData m1a1;
+    private Dictionary<Button, RangedWeaponData> weaponDict = new();
+
     private void Awake()
     {
         document = GetComponent<UIDocument>();
         root = document.rootVisualElement;
         shop = root.Q<VisualElement>("Shop");
         shopmenu = shop.Q<VisualElement>("ShopMenu");
-        root.style.opacity = 0;
-        document.sortingOrder = -1;
-        root.SetEnabled(false);
+        UI.SetActive(ref root, false);
     }
 
     private void Start()
     {
-        Button button = new();
-        button.AddToClassList("shop_buy_slot_button");
-        button.style.position = Position.Absolute;
-        button.style.width = 100;
-        button.style.height = 100;
-        button.style.marginBottom = 10;
-        button.style.marginTop = 10;
-        button.style.marginLeft = 10;
-        button.style.marginRight = 10;
-        button.style.paddingBottom = 10;
-        button.style.paddingTop = 10;
-        button.style.paddingLeft = 10;
-        button.style.paddingRight = 10;
-        shopmenu.Add(button);
+        CreateShopLine(out VisualElement line); shopmenu.Add(line);
+        CreateShopButton(out Button button, 30); line.Add(button);
+        CreateShopButton(out button, 30); line.Add(button); weaponDict[button] = m1a1;
+        CreateShopButton(out button, 30); line.Add(button);
+        CreateShopLine(out line); shopmenu.Add(line);
+        CreateShopButton(out button, 30); line.Add(button);
+        CreateShopButton(out button, 30); line.Add(button);
+        CreateShopButton(out button, 30); line.Add(button);
+        CreateShopLine(out line); shopmenu.Add(line);
+        CreateShopButton(out button, 30); line.Add(button);
+        CreateShopButton(out button, 30); line.Add(button);
+        CreateShopButton(out button, 30); line.Add(button);
+
+        List<Button> buttonList = shopmenu.Query<Button>().ToList();
+
+        foreach (Button btn in buttonList)
+        {
+            if (weaponDict.ContainsKey(btn))
+            {
+                Button btnRef = btn;
+                AddProductLabelsToShopButton(ref btnRef, weaponDict[btn].entityName, weaponDict[btn].price.ToString() + " $");
+                btn.clicked += () => Debug.Log(weaponDict[btn].entityName);
+                // On hover Debug Log entity name
+                btn.RegisterCallback<PointerEnterEvent>(evt => OnShopButtonEnter(btn));
+                btn.RegisterCallback<PointerLeaveEvent>(evt => OnShopButtonLeave());
+            }
+            else
+            {
+            }
+        }
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            root.style.opacity = root.style.opacity.value == 1 ? 0 : 1;
-            root.SetEnabled(!root.enabledInHierarchy);
-            document.sortingOrder = root.enabledInHierarchy ? 1 : -1;
+            UI.ToggleActive(ref root);
         }
     }
 
+    private void CreateShopButton(out Button button, float widthPercent)
+    {
+        button = new();
+        button.AddToClassList("shop_buy_slot_button");
+        button.style.position = Position.Relative;
+        button.style.width = UI.PercentLength(widthPercent);
+        UI.SetMargin(ref button, 10);
+        UI.SetPadding(ref button, 10);
+    }
+
+    private void CreateShopLine(out VisualElement line)
+    {
+        line = new();
+        line.style.flexDirection = FlexDirection.Row;
+        line.style.width = UI.PercentLength(100);
+        line.style.height = UI.PercentLength(30);
+        line.style.justifyContent = Justify.Center;
+        line.style.alignSelf = Align.Center;
+    }
+
+    private void AddLabelToShopButton(ref Button button, string text, float fontSize, TextAnchor anchor, Vector2 shift)
+    {
+        Label label = new(text);
+        label.style.color = Color.white;
+        label.style.unityTextAlign = anchor;
+        UI.SetMargin(ref label);
+        UI.SetPadding(ref label);
+        label.style.position = Position.Absolute;
+        UI.SetPosition(ref label, anchor, shift);
+        label.style.fontSize = fontSize;
+        button.Add(label);
+    }
+
+    private void AddProductLabelsToShopButton(ref Button button, string productName, string productPrice)
+    {
+        AddLabelToShopButton(ref button, productName, 30, TextAnchor.UpperRight, new(5, 5));
+        AddLabelToShopButton(ref button, productPrice, 30, TextAnchor.LowerRight, new(2, 5));
+    }
+
+    private void OnShopButtonEnter(Button button)
+    {
+        VisualElement statsElement = new() { name = "StatsElement" };
+        root.Add(statsElement);
+        UI.SetSize(ref statsElement, 200f, 200f);
+        statsElement.style.position = Position.Absolute;
+        UI.SetPosition(ref statsElement, TextAnchor.MiddleRight, 50f, 0f);
+        statsElement.style.backgroundColor = new Color(1, 1, 1, .1f);
+    }
+
+    private void OnShopButtonLeave()
+    {
+        root.Q<VisualElement>("StatsElement")?.RemoveFromHierarchy();
+    }
 }
