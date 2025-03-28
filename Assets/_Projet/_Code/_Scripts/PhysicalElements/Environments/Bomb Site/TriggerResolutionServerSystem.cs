d5037369cb3df6fbe6aac17204def09c6c6f94e1
Timotee Baboulin <timotee.baboulin@gmail.com>
2025-03-28 14:26:42 +0100

    You can only plant the bomb on the site now

--- Assets/_Projet/_Code/_Scripts/PhysicalElements/Environments/Bomb Site/TriggerResolutionServerSystem.cs
+++ Assets/_Projet/_Code/_Scripts/PhysicalElements/Environments/Bomb Site/TriggerResolutionServerSystem.cs
@@ -16,8 +16,6 @@ partial struct TriggerResolutionServerSystem : ISystem
 
     public void OnUpdate(ref SystemState state)
     {
-        //EntityCommandBuffer ecb = new EntityCommandBuffer(Allocator.Temp);
-
         if (!SystemAPI.TryGetSingleton<SimulationSingleton>(out var simulationSingleton))
         {
             Debug.Log("No physics found");
@@ -28,21 +26,19 @@ partial struct TriggerResolutionServerSystem : ISystem
             return;
         Simulation simulation = simulationSingleton.AsSimulation();
 
-        Debug.Log("1");
-
         foreach (var triggerEvent in simulation.TriggerEvents)
         {
-            if (SystemAPI.HasComponent<CharacterComponent>(triggerEvent.EntityA))
+            if (SystemAPI.HasComponent<CharacterComponent>(triggerEvent.EntityA)
+                && SystemAPI.HasComponent<BombSiteTriggerComponent>(triggerEvent.EntityB))
             {
                 SystemAPI.GetComponentRW<CharacterComponent>(triggerEvent.EntityA).ValueRW.isOnSite = true;
             }
-            else if (SystemAPI.HasComponent<CharacterComponent>(triggerEvent.EntityB))
+            else if (SystemAPI.HasComponent<CharacterComponent>(triggerEvent.EntityB)
+                && SystemAPI.HasComponent<BombSiteTriggerComponent>(triggerEvent.EntityA))
             {
                 SystemAPI.GetComponentRW<CharacterComponent>(triggerEvent.EntityB).ValueRW.isOnSite = true;
             }
         }
-
-        Debug.Log("2");
     }
 
     [BurstCompile]
--- Assets/_Projet/_Code/_Scripts/PhysicalElements/Harvester/HarvesterSystemServer.cs
+++ Assets/_Projet/_Code/_Scripts/PhysicalElements/Harvester/HarvesterSystemServer.cs
@@ -4,7 +4,6 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.NetCode;
 using Unity.Transforms;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using static RoundSystemServer;
 
