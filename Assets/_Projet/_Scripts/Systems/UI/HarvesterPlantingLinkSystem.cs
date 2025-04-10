781a84672850015de71c08b44d0d4c635df6fb42
Arca Théo <121396182+theodu30@users.noreply.github.com>
2025-04-10 13:37:00 +0200

    Defuse Correction

--- Assets/_Projet/_Scripts/Systems/UI/HarvesterPlantingLinkSystem.cs
+++ Assets/_Projet/_Scripts/Systems/UI/HarvesterPlantingLinkSystem.cs
@@ -4,7 +4,6 @@ using Unity.NetCode;
 
 partial class HarvesterPlantingLinkSystem : SystemBase
 {
-    // Events and Timer
     public class HarversterPlantRunning : EventArgs { public float time; public float maxTime; }
     public event EventHandler OnPlantStart;
     public event EventHandler<HarversterPlantRunning> OnPlantRunning;
@@ -17,16 +16,16 @@ partial class HarvesterPlantingLinkSystem : SystemBase
     }
     protected override void OnUpdate()
     {
-        // Get Current Tick
         var networkTime = SystemAPI.GetSingleton<NetworkTime>();
         NetworkTick currentTick = networkTime.InterpolationTick;
 
-        // On Harvester Planting Start and Running
         foreach (var (harvester, owner, entity) in SystemAPI
             .Query<RefRO<HarvesterComponent>, RefRO<StuffOwner>>()
             .WithAll<HarvesterPlanting>()
             .WithEntityAccess())
         {
+            if (owner.ValueRO.Value == Entity.Null) continue;
+            if (!EntityManager.IsComponentEnabled<GhostOwnerIsLocal>(owner.ValueRO.Value)) continue;
             if (timeSpent == 0f)
             {
                 OnPlantStart?.Invoke(this, EventArgs.Empty);
@@ -43,7 +42,6 @@ partial class HarvesterPlantingLinkSystem : SystemBase
             OnPlantRunning?.Invoke(this, new HarversterPlantRunning() { time = timeSpent, maxTime = 60 * 4 });
         }
 
-        // On Harvester Planting Cancel or End
         foreach (var (harvester, owner, entity) in SystemAPI
             .Query<RefRO<HarvesterComponent>, RefRO<StuffOwner>>()
             .WithDisabled<HarvesterPlanting>()
