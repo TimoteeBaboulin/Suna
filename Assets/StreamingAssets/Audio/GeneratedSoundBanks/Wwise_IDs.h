9a2304f71ffbda63c64176778ad1f9ac5262ca2e
ThomasC <136691121+Turrok@users.noreply.github.com>
2025-04-26 10:09:29 +0200

    Wwise prés integration (pour pull)

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,9 +13,20 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID BUTTON_CLIC = 2494499361U;
+        static const AkUniqueID BUTTON_RELEASE = 3732113631U;
+        static const AkUniqueID BUTTON_ROLLOVER = 3644104355U;
         static const AkUniqueID FOOTSTEP = 1866025847U;
+        static const AkUniqueID HARVESTER_MUSIC_END = 2788436815U;
+        static const AkUniqueID KILL = 2030840469U;
+        static const AkUniqueID MISS = 3062523241U;
         static const AkUniqueID PISTOL_RELOAD = 1068215004U;
         static const AkUniqueID PISTOL_SHOOT = 2124970216U;
+        static const AkUniqueID ROUND_START = 280570306U;
+        static const AkUniqueID SHOTGUN_COCK_BACK = 3750486034U;
+        static const AkUniqueID SHOTGUN_COCK_FORWARD = 3003651970U;
+        static const AkUniqueID SHOTGUN_SHOOT = 2040071657U;
+        static const AkUniqueID SHOTGUN_SHOOT_TEST = 3542188584U;
         static const AkUniqueID SMG_RELOAD = 2642532248U;
         static const AkUniqueID SMG_SHOOT = 1025907732U;
     } // namespace EVENTS
