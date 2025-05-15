a6b89b54adf10639571641afca7f52b3bcba3b61
Timotee Baboulin <timotee.baboulin@gmail.com>
2025-05-15 14:44:59 +0200

    Scrapped Decal System Remains

---
+++ Assets/_Projet/_Scripts/MonoBehaviours/DecalTimer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DecalTimer : MonoBehaviour
+{
+    [SerializeField] private float _timer;
+
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+        if (_timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
