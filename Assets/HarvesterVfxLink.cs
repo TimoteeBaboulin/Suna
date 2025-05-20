36f2ae119b210cb3d0025151742130beaa9e9ff8
Timotee Baboulin <timotee.baboulin@gmail.com>
2025-05-20 15:06:44 +0200

    Harvester is now correctly positioned (Pls don't break again)

--- Assets/HarvesterVfxLink.cs
+++ Assets/HarvesterVfxLink.cs
@@ -1,19 +1,58 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.VFX;
 
 public class HarvesterVfxLink : MonoBehaviour
 {
-    [SerializeField] private VisualEffect _vfx;
+    [SerializeField] private VisualEffect[] _vfx;
+    [SerializeField] private ParticleSystem[] _particles;
+    [SerializeField] private Transform _jTrans;
 
-    public void Play()
+    private float3 _pos;
+    private bool _playing = false;
+
+    public void Play(float3 pos)
     {
-        _vfx.Play();
-        _vfx.SetBool("IsAlive", true);
+        foreach (var vfx in _vfx)
+        {
+            vfx.Play();
+            vfx.SetBool("IsAlive", true);
+        }
+
+        foreach (var particle in _particles)
+        {
+            particle.Play();
+        }
+
+
+        _playing = true;
+        _jTrans.rotation = Quaternion.identity;
+        _jTrans.position = pos;
+        _pos = pos;
+    }
+
+    private void Update()
+    {
+        if (_playing)
+        {
+            _jTrans.position = _pos;
+            _jTrans.rotation = Quaternion.identity;
+        }
     }
 
     public void Stop()
     {
-        _vfx.Stop();
-        _vfx.SetBool("IsAlive", false);
+        foreach (var vfx in _vfx)
+        {
+            vfx.Stop();
+            vfx.SetBool("IsAlive", false);
+        }
+
+        foreach (var particle in _particles)
+        {
+            particle.Stop();
+        }
+
+        _playing = false;
     }
 }
