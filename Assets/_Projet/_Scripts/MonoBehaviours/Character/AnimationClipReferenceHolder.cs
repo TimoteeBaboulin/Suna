658db00c62123a8e319612c0d7507d7ed6fff99d
Smalleater <163728051+Smalleater@users.noreply.github.com>
2025-05-20 18:42:51 +0200

    Fix corpo tps grenade

---
+++ Assets/_Projet/_Scripts/MonoBehaviours/Character/AnimationClipReferenceHolder.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class AnimationClipReferenceHolder : MonoBehaviour
+{
+    [Header("Corpo Fps Grenade")]
+    [SerializeField] private AnimationClip[] corpoFpsBaseGrenade;
+    [SerializeField] private AnimationClip[] corpoFpsFireGrenade;
+    [SerializeField] private AnimationClip[] corpoFpsFlashGrenade;
+    [SerializeField] private AnimationClip[] corpoFpsGaseGrenade;
+    [SerializeField] private AnimationClip[] corpoFpsSmokeGrenade;
+
+    [Header("Corpo Fps Harvester")]
+    [SerializeField] private AnimationClip[] corpoFpsHarvester;
+
+    [Header("Corpo Fps Knife")]
+    [SerializeField] private AnimationClip[] corpoFpsKnife;
+
+    [Header("Corpo Fps Weapon")]
+    [SerializeField] private AnimationClip[] corpoFpsBanduka;
+    [SerializeField] private AnimationClip[] corpoFpsDecimator;
+    [SerializeField] private AnimationClip[] corpoFpsFakir;
+    [SerializeField] private AnimationClip[] corpoFpsLaksya;
+    [SerializeField] private AnimationClip[] corpoFpsLP17;
+    [SerializeField] private AnimationClip[] corpoFpsNelara;
+    [SerializeField] private AnimationClip[] corpoFpsSKAR18;
+
+    [Header("Corpo Tps Legs")]
+    [SerializeField] private AnimationClip[] corpoTpsLegs;
+
+    [Header("Corpo Tps Death")]
+    [SerializeField] private AnimationClip[] corpoTpsDeath;
+
+    [Header("Corpo Tps Grenade")]
+    [SerializeField] private AnimationClip[] corpoTpsBaseGrenade;
+    [SerializeField] private AnimationClip[] corpoTpsFlashGrenade;
+    [SerializeField] private AnimationClip[] corpoTpsGaseGrenade;
+    [SerializeField] private AnimationClip[] corpoTpsSmokeGrenade;
+
+    [Header("Corpo Tps Harvester")]
+    [SerializeField] private AnimationClip[] corpoTpsHarvester;
+
+    [Header("Corpo Tps Knife")]
+    [SerializeField] private AnimationClip[] corpoTpsKnife;
+
+    [Header("Corpo Tps Weapon")]
+    [SerializeField] private AnimationClip[] corpoTpsLaksya;
+    [SerializeField] private AnimationClip[] corpoTpsBanduka;
+    [SerializeField] private AnimationClip[] corpoTpsDecimator;
+    [SerializeField] private AnimationClip[] corpoTpsFakir;
+    [SerializeField] private AnimationClip[] corpoTpsLP17;
+    [SerializeField] private AnimationClip[] corpoTpsNelara;
+    [SerializeField] private AnimationClip[] corpoTpsSKAR18;
+}
