249e965c5cbb260fba3dbbd5efcfe51cbb9f1380
Turrok <136691121+Turrok@users.noreply.github.com>
2025-05-20 15:00:28 +0200

    AudioVoiceImplement

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,15 +13,9 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID A_UNDER_SIEGE = 2712994321U;
-        static const AkUniqueID B_UNDER_SEIGE = 154822826U;
-        static const AkUniqueID BLASPHEMOUS_AHEAD = 1242455402U;
         static const AkUniqueID BUTTON_CLIC = 2494499361U;
         static const AkUniqueID BUTTON_RELEASE = 3732113631U;
         static const AkUniqueID BUTTON_ROLLOVER = 3644104355U;
-        static const AkUniqueID CLEAR_FOR_NOW = 199080381U;
-        static const AkUniqueID CORPO_DIES = 2606506286U;
-        static const AkUniqueID CORPO_KILLED = 2907384284U;
         static const AkUniqueID CS_LIKE_COUNTER_CLOCKWISE = 4036144589U;
         static const AkUniqueID CS_LIKE_COUNTER_CLOCKWISE_STOP = 1779440320U;
         static const AkUniqueID CS_LIKE_MAIN_TITLE_LOOP = 2735489995U;
@@ -34,37 +28,24 @@ namespace AK
         static const AkUniqueID CS_LIKE_NATIVES_LOOP_STOP = 2301338566U;
         static const AkUniqueID CS_LIKE_ROBOTS_LOOP = 2598419022U;
         static const AkUniqueID CS_LIKE_ROBOTS_LOOP_STOP = 267023757U;
-        static const AkUniqueID CUT_RETREAT = 3713787143U;
-        static const AkUniqueID DEAD_ONE = 1848201790U;
         static const AkUniqueID FLASH = 1556996917U;
         static const AkUniqueID FOOTSTEP = 1866025847U;
-        static const AkUniqueID FORWARD_COVER = 2550757004U;
         static const AkUniqueID GAZ = 579662033U;
-        static const AkUniqueID GROUND_HELP = 539155014U;
         static const AkUniqueID HARVESTER_DEFUSED = 3131898918U;
         static const AkUniqueID HARVESTER_LOOP = 3940682342U;
         static const AkUniqueID HARVESTER_PLANT = 3382952939U;
         static const AkUniqueID HEADSHOT = 2632074263U;
-        static const AkUniqueID HERETIC = 3254572857U;
-        static const AkUniqueID HERETIC_GATHERING = 925603791U;
-        static const AkUniqueID HOLD_YOUR_GROUND_A = 3043506290U;
-        static const AkUniqueID HOLD_YOUR_GROUND_B = 3043506289U;
-        static const AkUniqueID I_SEE_MOVEMENT = 658043218U;
-        static const AkUniqueID INSIDE_GAS = 2201005391U;
         static const AkUniqueID KILL = 2030840469U;
+        static const AkUniqueID KILL_EVENT = 281209160U;
         static const AkUniqueID KNIFE_DRAW = 3526212285U;
         static const AkUniqueID KNIFE_SWING = 3430418271U;
-        static const AkUniqueID MACHINE_BROKEN = 3445137252U;
         static const AkUniqueID MISS = 3062523241U;
         static const AkUniqueID NATIVE_BUYPHASE = 298712718U;
         static const AkUniqueID NEAR_BULLET = 2962560402U;
         static const AkUniqueID NO_BULLET = 3728918479U;
-        static const AkUniqueID ONE_FALLS = 1421935024U;
-        static const AkUniqueID PATH_CLEARS = 4283776977U;
         static const AkUniqueID PISTOL_RELOAD = 1068215004U;
         static const AkUniqueID PISTOL_SHOOT = 2124970216U;
         static const AkUniqueID RAGDOLL = 4135087902U;
-        static const AkUniqueID REGROUP = 2072703347U;
         static const AkUniqueID RIFLE_RELOAD = 3214964847U;
         static const AkUniqueID RIFLE_SHOOT = 1117295805U;
         static const AkUniqueID SHOTGUN_COCK_BACK = 3750486034U;
@@ -74,26 +55,12 @@ namespace AK
         static const AkUniqueID SMG_RELOAD = 2642532248U;
         static const AkUniqueID SMG_SHOOT = 1025907732U;
         static const AkUniqueID SMOKE = 1359218976U;
-        static const AkUniqueID SMOKE_BLIENDED = 2121643326U;
-        static const AkUniqueID SNIPER = 2550763292U;
         static const AkUniqueID SNIPER_RELOAD = 2515054376U;
         static const AkUniqueID SNIPER_SHOOT = 2801873252U;
-        static const AkUniqueID SPLIT_STRIKE = 1275552404U;
-        static const AkUniqueID STAY_PATIENT = 4176632124U;
         static const AkUniqueID STOPALL = 3086540886U;
         static const AkUniqueID STOPALL_01 = 4256069028U;
-        static const AkUniqueID TAKE_WEAPON = 3095820231U;
-        static const AkUniqueID THE_WIND_CARRIES = 2536847095U;
-        static const AkUniqueID THEY_RUSH = 2675578812U;
-        static const AkUniqueID THEYRE_COMING = 3995945122U;
         static const AkUniqueID THROWING_FLASH = 1781840702U;
         static const AkUniqueID THROWING_SMOKE = 3127593203U;
-        static const AkUniqueID TOGETHER_WE_PUSH = 3490572673U;
-        static const AkUniqueID TOO_MANY = 1605848557U;
-        static const AkUniqueID WAR_NOT_OVER = 209399956U;
-        static const AkUniqueID WATCH_FLANK = 316685025U;
-        static const AkUniqueID WE_STAND = 16774024U;
-        static const AkUniqueID WILL = 1570616273U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
